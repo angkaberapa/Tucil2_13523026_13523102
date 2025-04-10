75daecffa24555691452e4b3f1cf8fba15e47004
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2025-04-10 19:00:53 +0700

    feat: ImageData class to store all data: image data, parameter input, compression statistics and to load and save image

---
+++ src/ImageData.java
@@ -0,0 +1,68 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ImageData {
+    // Image data
+    public static int[][][] imageRGB;
+    public static int imageWidth;
+    public static int imageHeight;
+
+    // Parameter input
+    public static double threshold;
+    public static int minBlockSize;
+    public static int methodErrorCalculation; // 1: variance, 2: MAD, 3: max diff, 4: entropy, 5: SSIM
+    public static double targetCompressionRate; // 0 = no target, 1.0 = 100% (no compression), 0.5 = 50% (half size), etc.
+
+    // Statistik hasil kompresi
+    public static int totalNodes = 0;
+    public static int maxDepth = 0;
+    public static double originalSize;
+    public static double compressedSize;
+
+    public static void loadImage(String path) {
+        try {
+            File file = new File(path);
+            originalSize = file.length() / 1024; // in kB
+            BufferedImage image = ImageIO.read(file);
+            imageWidth = image.getWidth();
+            imageHeight = image.getHeight();
+            imageRGB = new int[imageHeight][imageWidth][3];
+
+            for (int y = 0; y < imageHeight; y++) {
+                for (int x = 0; x < imageWidth; x++) {
+                    int pixel = image.getRGB(x, y);
+                    imageRGB[y][x][0] = (pixel >> 16) & 0xFF; // R
+                    imageRGB[y][x][1] = (pixel >> 8) & 0xFF;  // G
+                    imageRGB[y][x][2] = (pixel) & 0xFF;       // B
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Gagal membaca file gambar: " + e.getMessage());
+        }
+    }
+    public static void saveImage(String path) {
+        BufferedImage output = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                int r = imageRGB[y][x][0];
+                int g = imageRGB[y][x][1];
+                int b = imageRGB[y][x][2];
+                int rgb = (r << 16) | (g << 8) | b;
+                output.setRGB(x, y, rgb);
+            }
+        }
+
+        try {
+            File outputFile = new File(path);
+            ImageIO.write(output, "jpg", outputFile);
+            System.out.println("Gambar berhasil disimpan ke: " + path);
+            compressedSize = outputFile.length() / 1024; // in kB
+        } catch (IOException e) {
+            System.out.println("Gagal menyimpan gambar: " + e.getMessage());
+        }
+    }
+}
