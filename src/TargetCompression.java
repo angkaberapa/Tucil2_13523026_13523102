bf6b94e14c854b170484e0d2773596655c915d58
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2025-04-11 16:45:44 +0700

    style: add comments

--- src/TargetCompression.java
+++ src/TargetCompression.java
@@ -1,6 +1,6 @@
 public class TargetCompression {
     private static double left = 0;
-    private static double right = 48768.75; // 
+    private static double right = 48768.75; // max error
     private static int[][][] closestImageRGB;
     private static int[][][] tempImageRGB;
     private static double closestCompressionRate = 100;
@@ -11,17 +11,16 @@ public class TargetCompression {
     private static int iteration = 0;
 
     public static void compressToTargetPercentage(){
-        // target compression = ImageData.targetCompressionRate
+        // target compression adalah ImageData.targetCompressionRate
         left = 0;
         right = 48768.75; 
         closestImageRGB = new int[ImageData.imageHeight][ImageData.imageWidth][3];
         tempImageRGB = new int[ImageData.imageHeight][ImageData.imageWidth][3];
-        // copy ImageData.imageRGB to tempImageRGB using systemarray.copy
         Utils.copyImage(ImageData.imageRGB, tempImageRGB);
         ImageData.totalNodes = 0;
         ImageData.maxDepth = 0;
 
-        // Binary search for the closest compression rate
+        // Binary search 
         while (left <= right) {
             iteration++;
             // reset ImageData.imageRGB
@@ -29,15 +28,18 @@ public class TargetCompression {
             double mid = (left + right) / 2;
             ImageData.threshold = mid;
 
-            // Create and divide the quadtree
+            // Kompresi gambar sesuai threshold
             QuadTreeNode root = new QuadTreeNode(0, 0, ImageData.imageWidth, ImageData.imageHeight);
             root.divide(0);
             ImageData.saveImage("temporary.jpg");
 
+            // Hasil kompresi
             tempCompressionRate = 100.0 * (1 - ((double) ImageData.compressedSize / ImageData.originalSize));
             System.out.println("Iterasi " + iteration + ":");
             System.out.println("Compression Rate: " + tempCompressionRate + "%");
             System.out.println("Threshold: " + ImageData.threshold);
+
+            // Mencari hasil dengan target kompresi terdekat
             if (Math.abs(tempCompressionRate - ImageData.targetCompressionRate) < Math.abs(closestCompressionRate - ImageData.targetCompressionRate)) {
                 closestCompressionRate = tempCompressionRate;
                 mostOptimalThreshold = ImageData.threshold;
@@ -47,9 +49,9 @@ public class TargetCompression {
             }
 
             if (tempCompressionRate < ImageData.targetCompressionRate) {
-                // naikin treshold
+                // treshold >> 
                 left = mid + 1;
-            } else { // turunin treshold
+            } else { // treshold <<
                 right = mid - 1;
             }
         }
@@ -58,7 +60,5 @@ public class TargetCompression {
         ImageData.threshold = mostOptimalThreshold;
         ImageData.totalNodes = tempTotalNodes;
         ImageData.maxDepth = tempMaxDepth;
-        // Save the closest image
-        ImageData.saveImage("compressed_closest.png");
     }
 }
